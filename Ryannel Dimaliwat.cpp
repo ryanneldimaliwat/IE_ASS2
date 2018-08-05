a22037665774c9083d53d9781a5a99c74d405895
ryanneldimaliwat <42083109+ryanneldimaliwat@users.noreply.github.com>
2018-08-05 13:13:54 +0800

    Add files via upload

---
+++ Ryannel Dimaliwat.cpp
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int one, two, three, four;
+
+    cout << "Hi! Welcome to the Program!"<< endl;
+
+    cout << "This program is designed on how to make brownies"<<endl;
+
+    cout << "To get started, please enter any number to view the steps"<<endl;
+
+    cin>>one;
+
+    cout<<"Thank you, Step 1. Preheat oven to 350 degrees F. Grease and flour an 8-inc square pan"<<endl;
+    cin>>two;
+
+
+    cout<<"Step 2. In a large saucepan, melt 1/2 cup butter. Remove from heat, and stir in sugar, eggs, and 1 teaspoon vanilla. Beat in 1/3 cup cocoa, 1/2 cup flour, salt, and baking powder. Spread batter into prepared pan"<<endl;
+    cin>>three;
+
+    cout<<"Step 3. Bake in preheated oven for 25 to 30 minutes. Do not over cook"<<endl;
+    cin>>four;
+
+    cout<<"Step 4. To make Frosting: Combine 3 tablespoons softened butter, 3 tablespoons cocoa, honey, 1 teaspoon vanilla extract, and 1 cup confectioners sugar. Stir until smooth. Frost brownies while they are still warm" <<endl;
+
+
+
+
+    cout<<"Thank you" <<endl;
+    system("PAUSE");
+
+
+
+    return 0;
+}
